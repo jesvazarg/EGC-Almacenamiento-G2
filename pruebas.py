453e26d6334af90a7a087a20110bf9fad54ff4df
martus13 <vidriosoft@gmail.com>
2017-12-21 00:56:07 +0100

    Archivo pruebas unit: añadido nuevos metodos al archivo de pruebas

--- pruebas.py
+++ pruebas.py
@@ -10,3 +10,19 @@ if __name__ == '__main__':
 
     prueba_negativa = get_voto(db, "123", "1") # Error, no existe el token
     print prueba_negativa
+
+    # Pruebas para el metodo get_voto_pregunta
+
+    prueba_positiva = get_voto(db, "1", "1")  # Prueba positiva
+    print prueba_positiva
+
+    prueba_negativa = get_voto(db, "123", "1")  # Error, no existe el token
+    print prueba_negativa
+
+    # Pruebas para el metodo comprobar_token
+
+    prueba_positiva = get_voto(db, "1", "1")  # Prueba positiva
+    print prueba_positiva
+
+    prueba_negativa = get_voto(db, "123", "1")  # Error, no existe el token
+    print prueba_negativa
