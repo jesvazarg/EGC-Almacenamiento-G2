6d0ca959123994033e9f22e2d25318af061a659d
Elena Camero <ecamero@Victors-MacBook-Air.local>
2017-12-21 01:11:58 +0100

    Pruebas API test: Pruebas para los metodos comprobar voto y comprobar voto pregunta.

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,5 @@
 from database import *
+import requests
 
 if __name__ == '__main__':
     db = conectar_db()
@@ -51,3 +52,54 @@ if __name__ == '__main__':
 
     prueba_negativa = get_voto(db, "123", "1")  # Error, no existe el token
     print prueba_negativa
+
+
+    # Pruebas para el metodo comprobar voto
+
+    comprobar_voto = requests.get("http://127.0.0.1:5000/get/comprobar_voto/QWERTY12345/1/1")
+    comprobar_voto_sin_token = requests.get("http://127.0.0.1:5000/get/comprobar_voto/1/1/1")
+    comprobar_voto_respuesta_vacia = requests.get("http://127.0.0.1:5000/get/comprobar_voto/QWERTY12345/19/1")
+
+    print "Comprobar voto (Llamada Correcta)"
+
+    print comprobar_voto
+    print comprobar_voto.text
+
+    print "Comprobar voto (Sin token)"
+
+    print comprobar_voto_sin_token
+    print comprobar_voto_sin_token.text
+
+    print "Comprobar voto (Respuesta vacia)"
+
+    print comprobar_voto_respuesta_vacia
+    print comprobar_voto_respuesta_vacia.text
+
+    # Pruebas para el metodo comprobar voto pregunta
+
+    comprobar_voto_pregunta = requests.get("http://127.0.0.1:5000/get/comprobar_voto_pregunta/QWERTY12345/1/1/1")
+    comprobar_voto_pregunta_sin_token = requests.get("http://127.0.0.1:5000/get/comprobar_voto_pregunta/1/1/1/1")
+    comprobar_voto_pregunta_respuesta_vacia = requests.get("http://127.0.0.1:5000/get/comprobar_voto_pregunta/QWERTY12345/19/1/1")
+
+    print "Comprobar voto pregunta (Llamada Correcta)"
+
+    print comprobar_voto_pregunta
+    print comprobar_voto_pregunta.text
+
+    print "Comprobar voto pregunta (Sin token)"
+
+    print comprobar_voto_pregunta_sin_token
+    print comprobar_voto_pregunta_sin_token.text
+
+    print "Comprobar voto pregunta (Respuesta vacía)"
+
+    print comprobar_voto_pregunta_respuesta_vacia
+    print comprobar_voto_pregunta_respuesta_vacia.text
+
+
+
+
+
+
+
+
